b3b89590419c50319b026429303695de4372b789
iffok247 <s.assougnon@gmail.com>
2025-04-16 20:54:43 +0200

    fonction generation uuid

--- controller.js
+++ controller.js
@@ -1,4 +1,3 @@
-
 var editingMode = { rect: 0, line: 1 };
 
 function Pencil(ctx, drawing, canvas) {
@@ -42,5 +41,14 @@ function Pencil(ctx, drawing, canvas) {
 		this.currentShape.paint(ctx);
 	  }.bind(this)
 };
+
+// Fonction pour générer un UUID unique @see https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
+function generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = Math.random() * 16 | 0;
+        const v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
 
 
